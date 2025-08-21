67d33cc7bb1666ba99da839b66630007be41d8e8
Carl-Fredrik Rif <carl-fredrik.rif.data24hel@edu.tucsweden.se>
2025-08-21 11:05:48 +0200

    separated login info from main code

---
+++ connect-to-db.js
@@ -0,0 +1,37 @@
+// Import the database driver
+import mysql from 'mysql2/promise';
+import { readFile } from 'fs/promises';
+
+// Read the JSON file manually
+const jsonText = await readFile('./connection.json', 'utf-8');
+const data = JSON.parse(jsonText);
+
+console.log(`hostname ${data.host}`); // output 'testing'
+
+// Create a connection 'db' to the database
+const db = await mysql.createConnection({
+    host: data.host, 
+    port: data.port,
+    user: data.user, 
+    password: data.password,
+    database: data.database
+});
+
+// A small function for a query
+async function query(sql){
+   let result = await db.execute(sql);
+   return result[0];
+}
+
+// let allPersons = await query('SELECT * FROM persons');
+
+// // Show the result
+// console.log('allPersons',allPersons);
+
+// let personsWithLongerNames = await query(`
+//     SELECT * 
+//     FROM persons 
+//     WHERE LENGTH(name) >= 10
+// `);
+
+// console.log('personsWithLongerNames', personsWithLongerNames);
