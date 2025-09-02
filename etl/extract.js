bfbd2078205af635b98186740cfcf8e172d6f6d7
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-09-02 16:55:04 +0200

    first draft finished

---
+++ etl/extract.js
@@ -0,0 +1,24 @@
+import { readFile } from 'node:fs/promises';
+import { basename } from 'node:path';
+
+// This file a submodule for index-etl.js
+// Purpose is to extract and later retrieve metadata from a given set of files
+
+export async function extract(filePath) {
+    try {
+        // for now we have a ready .json-file
+        // 
+        const content = await readFile(filePath, 'utf-8');
+        const jsonContent = JSON.parse(content);
+        const filename = basename(filePath);
+
+        return {
+            filename,
+            category: 'pdf',
+            content: jsonContent
+        };
+    } catch (error) {
+        console.error('Error in extract:', error);
+        throw error;
+    }
+}
