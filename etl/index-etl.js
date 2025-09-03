b266764df5b469de3ad7957b2a1246ed1c15e063
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-09-03 23:59:03 +0200

    now able to display category from input file

--- etl/index-etl.js
+++ etl/index-etl.js
@@ -1,7 +1,16 @@
 import { extract } from './extract.js';
 import { transform } from './transform.js';
 
-const filePath = 'csv/pdf_metadata_1756802965122.json'; // Replace with your file path
+// Replace with your file path
+const CSV_DIRECTORY = 'csv/'; // Relative to this file location
+const CSV_FILENAME = 'pdf_metadata_1756802965122.json'; // filename in directory
+
+const filePath = CSV_DIRECTORY + CSV_FILENAME;
+
+const first_ = CSV_FILENAME.indexOf("_")
+const category = CSV_FILENAME.substring(0, first_)
+
+// console.log(category)
 
 async function runETL() {
     try {
@@ -11,10 +20,13 @@ async function runETL() {
         // Stage 2: Transform
         transform(extractedData);
 
+        // Stage 3: Load
+        
+
         console.log('ETL process completed!');
     } catch (error) {
         console.error('ETL process failed:', error);
     }
 }
 
-runETL();
+// runETL();
