51566e7e727e9a639c02a751894fc6177722ca92
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-09-08 00:38:12 +0200

    unknown upgrades

--- retrieval/retrieve.js
+++ retrieval/retrieve.js
@@ -1,14 +1,15 @@
-import { search, SafeSearchType, searchImages } from 'duck-duck-scrape';
-// import * as DDG from 'duck-duck-scrape';
+import axios from 'axios';
 
-const searchQuery = "hej";
+async function duckDuckGoSearch(query) {
+  const response = await axios.get('https://duckduckgo.com/', {
+    params: { q: query },
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    }
+  });
+  return response.data;
+}
 
-// const searchResults = await search(searchQuery, {
-//   safeSearch: SafeSearchType.OFF
-// });
-
-const searchResults = await search(searchQuery,
-    SafeSearchType.OFF
-);
-
-console.log(searchResults);
+const searchQuery = "rapport hej pdf";
+const results = await duckDuckGoSearch(searchQuery);
+console.log(results);
