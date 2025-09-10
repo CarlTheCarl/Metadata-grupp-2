5726dcd056b551172dc4e33183346e3d36ee1b0d
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2025-09-10 14:35:14 +0200

    started on picture metadata extraction

--- etl/etl-pictures/extract.js
+++ etl/etl-pictures/extract.js
@@ -1,30 +1,83 @@
-import { readFile } from 'node:fs/promises';
-import { basename } from 'node:path';
+// import exifr - a metadata extractor for images
+import exifr from 'exifr';
+// import fs (file system) - a built in module in Node.js
+import fs from 'fs';
+import { type } from 'os';
+import { join } from 'path';
 
-// This file a submodule for index-etl.js
-// Purpose is to extract and later retrieve metadata from a given set of files
+// defaults
+const default_directory = 'retrieval/get_img_metadata/images';
 
-let content = "";
+// export async function getListOfFiles(directory){
+//     // give me a list of all files in the image folder
+//     let images = fs.readdirSync(directory);
 
-export async function extract(filePath, datatype) {
-    try {
-        if (datatype == "JSON") {
-            jsonRead = await readFile(filePath, 'utf-8');
-            content = JSON.parse(jsonRead);
-        } else if (datatype == "csv"){
-            content = await readFile(filePath, 'utf-8');
+//     return images;
+// }
+
+// const images = getListOfFiles('retrieval/get_img_metadata/images');
+
+
+// Loop through the images and extract the metadata
+export async function extractMetadata(directory) {
+    let numberOfUndefined = 0;
+    let numberOfMetadataFiles = 0;
+    let whatTheDuckIsThis = 0;
+
+    for (let image of fs.readdirSync(directory)) {
+        try {
+            // Only for files ending with .jpg
+            // slice(-4) get the last 4 letters from the image name
+            if (image.slice(-4) == '.jpg') {
+                console.log('IMAGE: ' + image);
+                let metadata = await exifr.parse(join(directory, image));
+                if (metadata == undefined) {
+                    numberOfUndefined += 1;
+                } else if (metadata != undefined) {
+                    numberOfMetadataFiles += 1;
+                    // console.log(metadata);
+                } else {
+                    whatTheDuckIsThis += 1;
+                }
+            }
+        } catch(err) {
+            console.log(err);
         }
-
-        const filename = basename(filePath);
-
-        console.log(filename, content)
-
-        return {
-            filename,
-            content: content
-        };
-    } catch (error) {
-        console.error('Error in extract:', error);
-        throw error;
     }
+    console.log("Number of undefined = ", numberOfUndefined);
+    console.log("Number of defined = ", numberOfMetadataFiles);
+    console.log("Number of WTDIS = ", whatTheDuckIsThis);
 }
+
+await extractMetadata(default_directory);
+
+// export async function retrieve_exif_from_jpg(directory, list_of_files) {
+//     let metadata = {};
+
+
+//     // Loop through the images and extract the metadata
+//     for (let image of list_of_files) {
+//         metadata['filename'] = image;
+//         console.log(metadata["filename"]);
+//         // Only for files ending with .jpg
+//         // slice(-4) get the last 4 letters from the image name
+//         if (image.slice(-4) == '.jpg') {
+//             let metadata = await exifr.parse(join(directory, image));
+//         }
+//     }
+
+//     console.log(metadata);
+// }
+
+// export async function retrieveAllMetadata(directory) {
+//     console.log("directory: ", directory);
+//     const listOfFiles = await getListOfFiles(directory);
+//     console.log(typeof(listOfFiles[0]));
+
+//     const retrieved_metadata = await retrieve_exif_from_jpg(directory, listOfFiles);
+
+//     // return retrieved_metadata;
+    
+// }
+
+// console.log(retrieveAllMetadata("retrieval/get_img_metadata/images"));
